093ebeff173d808adf07659c1b5a49580f32bccb
William-Misiaszek <wmisiasz@gmail.com>
2023-06-16 14:00:53 -0700

    Alex V1

---
+++ app/controllers/social_controller.rb
@@ -0,0 +1,4 @@
+class SocialController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   root 'friends#index'
   get "home/friends"
   resources :friends
+  get 'portfolio', to: 'portfolio#index', as: 'portfolio'
+  get 'social', to: 'social#index', as: 'social'
+  get 'contact', to: 'contact#index', as: 'contact'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
