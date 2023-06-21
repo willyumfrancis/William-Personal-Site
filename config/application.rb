0c1473471ea04b319bb74aae25b619d00560cffd
William-Misiaszek <wmisiasz@gmail.com>
2023-06-20 19:22:20 -0700

    Heroku v3

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Ruby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.paths << Rails.root.join('node_modules')
+
 
     # Configuration for the application, engines, and railties goes here.
     #
