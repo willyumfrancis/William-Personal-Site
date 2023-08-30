a570700b43527a2627e4b6244b36ec253eeae271
William-Misiaszek <wmisiasz@gmail.com>
2023-08-29 22:27:43 -0700

    v4.3

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -1,5 +1,6 @@
 # app/controllers/transcriptions_controller.rb
 require 'httparty'
+require 'json'
 
 class TranscriptionsController < ApplicationController
   skip_before_action :verify_authenticity_token # Skip CSRF check for API
@@ -14,24 +15,32 @@ class TranscriptionsController < ApplicationController
     model_name = "whisper-1"
     response_format = "json" # or "text" based on your needs
     
-    response = HTTParty.post(
-      'https://api.openai.com/v1/audio/transcriptions', # Confirm this URL is correct
-      headers: { "Authorization" => "Bearer #{ENV['WHISPER_API_KEY']}" },
-      body: { 
-        file: audio_file.read,
-        model: model_name,
-        response_format: response_format
-      }
-    )
+    begin
+      response = HTTParty.post(
+        'https://api.openai.com/v1/audio/transcriptions',
+        headers: { "Authorization" => "Bearer #{ENV['WHISPER_API_KEY']}" },
+        body: { 
+          file: audio_file.read,
+          model: model_name,  # Enclosed in quotes
+          response_format: response_format
+        }
+      )
 
-    puts "Whisper API response: #{response.body}"
+      puts "Whisper API response: #{response.body}"
 
-    if response.code == 200
-      transcription = JSON.parse(response.body)['text'] # Adjust based on actual response key
-      render json: { transcription: transcription }, status: :ok
-    else
-      puts "Failed to transcribe audio, status code: #{response.code}"
-      render json: { error: 'Failed to transcribe audio' }, status: :bad_request
+      if response.code == 200
+        transcription = JSON.parse(response.body)['text'] # Adjust based on actual response key
+        render json: { transcription: transcription }, status: :ok
+      else
+        puts "Failed to transcribe audio, status code: #{response.code}"
+        render json: { error: 'Failed to transcribe audio' }, status: :bad_request
+      end
+    rescue HTTParty::Error => e
+      puts "HTTParty Error: #{e.message}"
+      render json: { error: "HTTParty Error: #{e.message}" }, status: :bad_request
+    rescue StandardError => e
+      puts "Standard Error: #{e.message}"
+      render json: { error: "Standard Error: #{e.message}" }, status: :bad_request
     end
   end
 end
