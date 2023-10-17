cad7ed151fdb6101175b02c80f7b8c078b2521a4
William-Misiaszek <wmisiasz@gmail.com>
2023-10-17 00:30:49 -0700

    v2 willfrancisco

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,6 @@ class SearchController < ApplicationController
     def google
         query = params[:query]
         search_engine_id = "a6ccd2b24e9284464"
-        redirect_to "https://www.google.com/search?q=site:tanchiniproductions.com%20#{query}&cx=#{search_engine_id}", allow_other_host: true
+        redirect_to "https://www.google.com/search?q=site:willfrancisco.com%20#{query}&cx=#{search_engine_id}", allow_other_host: true
     end
 end
