f6554956bd346e60393b9d927ea757bdeff6ded0
William-Misiaszek <wmisiasz@gmail.com>
2023-10-17 00:29:20 -0700

    PSE T7

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,9 @@
-# app/controllers/search_controller.rb
+
+
 class SearchController < ApplicationController
     def google
         query = params[:query]
-        search_engine_id = "a6ccd2b24e9284464"  # Updated search engine ID
-        encoded_query = URI.encode(query)
-        redirect_url = "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}"
-        redirect_to redirect_url, allow_other_host: true
+        search_engine_id = "a6ccd2b24e9284464"
+        redirect_to "https://www.google.com/search?q=site:tanchiniproductions.com%20#{query}&cx=#{search_engine_id}", allow_other_host: true
     end
 end
