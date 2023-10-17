07b9b16a402b5f3677b07cafea60d45836cd7a54
William-Misiaszek <wmisiasz@gmail.com>
2023-10-16 21:22:21 -0700

    Updated log for PSE

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,14 @@
+# app/controllers/search_controller.rb
 class SearchController < ApplicationController
     def google
         query = params[:query]
         search_engine_id = "80523033ec3b94762"
         encoded_query = URI.encode(query)
-        redirect_to "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}", allow_other_host: true
+        redirect_url = "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}"
+        logger.info "Redirecting to: #{redirect_url}"
+        redirect_to redirect_url, allow_other_host: true
     rescue => e
         logger.error e.message
         logger.error e.backtrace.join("\n")
-        # Optionally, render a custom error page
-        # render 'error_page'
     end
 end
