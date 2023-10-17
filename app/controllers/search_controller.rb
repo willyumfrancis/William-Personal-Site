a4c7e72d08fa53a1ae6a6bd10e3076e45a03056f
William-Misiaszek <wmisiasz@gmail.com>
2023-10-16 21:45:14 -0700

    go

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,9 @@
 class SearchController < ApplicationController
     def google
         query = params[:query]
-        search_engine_id = "80523033ec3b94762"
+        search_engine_id = "a6ccd2b24e9284464"  # Updated search engine ID
         encoded_query = URI.encode(query)
         redirect_url = "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}"
-        logger.info "Redirecting to: #{redirect_url}"
         redirect_to redirect_url, allow_other_host: true
-    rescue => e
-        logger.error e.message
-        logger.error e.backtrace.join("\n")
     end
 end
