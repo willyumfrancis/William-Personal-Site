f580c31ca6cb2bd2294f0f14d981633e7a8b6bb2
William-Misiaszek <wmisiasz@gmail.com>
2023-10-16 21:04:35 -0700

    Hello

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,12 @@ class SearchController < ApplicationController
     def google
         query = params[:query]
         search_engine_id = "80523033ec3b94762"
-        # Ensure the query is URL-encoded to prevent issues with special characters
         encoded_query = URI.encode(query)
         redirect_to "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}", allow_other_host: true
+    rescue => e
+        logger.error e.message
+        logger.error e.backtrace.join("\n")
+        # Optionally, render a custom error page
+        # render 'error_page'
     end
 end
