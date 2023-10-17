76d882ba5be1673e0911de5dce2153f0c825cd42
William-Misiaszek <wmisiasz@gmail.com>
2023-10-16 20:53:55 -0700

    Update PSE Google

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
     def google
         query = params[:query]
         search_engine_id = "80523033ec3b94762"
-        redirect_to "https://www.google.com/search?q=site:willfrancisco.com%20#{query}&cx=#{search_engine_id}", allow_other_host: true
+        # Ensure the query is URL-encoded to prevent issues with special characters
+        encoded_query = URI.encode(query)
+        redirect_to "https://cse.google.com/cse?cx=#{search_engine_id}&q=#{encoded_query}", allow_other_host: true
     end
 end
